9af932970c9161b4aa60c5023dfdd5f5a07468ff
Asmi jain <asmidev29@gmail.com>
2023-01-29 15:21:35 +0530

    Initial Commit

---
+++ badge/badgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  badgeApp.swift
+//  badge
+//
+//  Created by ASMI iOS Dev on 29/01/23.
+//
+
+import SwiftUI
+
+@main
+struct badgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
